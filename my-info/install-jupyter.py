56f24d970757de661cde51f8ec8c9414d815691d
jims57 <jims57@163.com>
2025-07-07 18:06:31 +0800

    ok

--- my-info/install-jupyter.py
+++ my-info/install-jupyter.py
@@ -1,28 +1,23 @@
-import subprocess
-import os
+#!/bin/bash
 
 # Update package list
-subprocess.run(['apt', 'update'], check=True)
+apt update
 
 # Install python3-pip
-subprocess.run(['apt', 'install', '-y', 'python3-pip'], check=True)
+apt install -y python3-pip
 
 # Install jupyter
-subprocess.run(['pip3', 'install', 'jupyter'], check=True)
+pip3 install jupyter
 
 # Create startup script
-startup_script = '''#!/bin/bash
+cat > /root/.jupyter_startup.sh << 'EOF'
+#!/bin/bash
 nohup jupyter notebook --ip=0.0.0.0 --port=9000 --no-browser --allow-root --notebook-dir=/ > /var/log/jupyter.log 2>&1 &
 echo "Jupyter Notebook started in background on port 9000"
-'''
-
-# Write startup script to file
-with open('/root/.jupyter_startup.sh', 'w') as f:
-    f.write(startup_script)
+EOF
 
 # Make script executable
-os.chmod('/root/.jupyter_startup.sh', 0o755)
+chmod +x /root/.jupyter_startup.sh
 
 # Add to bashrc
-with open('/root/.bashrc', 'a') as f:
-    f.write('\n/root/.jupyter_startup.sh\n')
+echo "/root/.jupyter_startup.sh" >> /root/.bashrc
